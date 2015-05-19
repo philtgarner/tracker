ae43e2c0340af0d1cd0dff88e9ed86be1d253211
Phil Garner <info@philipgarner.co.uk>
2015-05-19 20:39:29 +0100

    Fixed NaN in distance calculation
    The distance calculation was returning NaN when there was no movement
    between the given two points. Now performs this check and returns 0 if
    no movement.

--- resources/connect.php
+++ resources/connect.php
@@ -30,13 +30,18 @@ try{
 }
 
 function distance($lat1, $lon1, $lat2, $lon2) {
-	$theta = $lon1 - $lon2;
-	$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-	$dist = acos($dist);
-	$dist = rad2deg($dist);
-	$miles = $dist * 60 * 1.1515;
+	if($lat1 == $lat2 && $lon1 == $lon2){
+		return 0;
+	}
+	else{
+		$theta = $lon1 - $lon2;
+		$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+		$dist = acos($dist);
+		$dist = rad2deg($dist);
+		$miles = $dist * 60 * 1.1515;
 
-	return ($miles * 1.609344);
+		return ($miles * 1.609344);
+	}
 }
 
 ?>
